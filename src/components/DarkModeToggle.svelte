b1372c429a59721f086d0aebfbc59a0784e9e626
0t4u <61939142+0t4u@users.noreply.github.com>
2024-04-06 03:53:28 +0100

    fix: stop transition when changing theme

--- src/components/DarkModeToggle.svelte
+++ src/components/DarkModeToggle.svelte
@@ -4,11 +4,29 @@
 
     if (typeof window !== "undefined") {
         theme.subscribe(v => {
+            const css = document.createElement("style")
+            css.appendChild(
+                document.createTextNode(
+                    `* {
+                    -webkit-transition: none !important;
+                    -moz-transition: none !important;
+                    -o-transition: none !important;
+                    -ms-transition: none !important;
+                    transition: none !important;
+                    }`
+                )
+            );
+
+            document.head.appendChild(css);
+
             if (v === "dark" || (v === "device") && window.matchMedia("(prefers-color-scheme: dark)").matches) {
                 document.documentElement.classList.add("dark");
             } else {
                 document.documentElement.classList.remove("dark");
             }
+
+            const _ = window.getComputedStyle(css).opacity
+            document.head.removeChild(css)
         });
     }
 </script>
