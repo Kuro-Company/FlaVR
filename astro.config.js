efcf8b85b4bf1d886c9feeaf49990096eb5a7c27
0t4u <61939142+0t4u@users.noreply.github.com>
2024-09-22 14:46:44 +0100

    feat: add mdx

--- astro.config.js
+++ astro.config.js
@@ -1,37 +1,42 @@
 import { defineConfig } from "astro/config";
 import svelte from "@astrojs/svelte";
 import tailwind from "@astrojs/tailwind";
+import mdx from "@astrojs/mdx";
 import icon from "astro-icon";
 import paraglide from "@inlang/paraglide-js-adapter-astro";
 import { purgeCss } from "vite-plugin-tailwind-purgecss";
 
 // https://astro.build/config
 export default defineConfig({
-    trailingSlash: "always",
-    i18n: {
-        locales: ["en", "es", "ja"],
-        defaultLocale: "en",
-        routing: {
-            prefixDefaultLocale: true,
-            redirectToDefaultLocale: false,
-        },
+  trailingSlash: "always",
+  i18n: {
+    locales: ["en", "es", "ja"],
+    defaultLocale: "en",
+    routing: {
+      prefixDefaultLocale: true,
+      redirectToDefaultLocale: false,
     },
-    redirects: {
-        "/": {
-            status: 302,
-            destination: "/en/",
-        },
+  },
+  redirects: {
+    "/": {
+      status: 302,
+      destination: "/en/",
     },
-    vite: {
-        build: {
-            sourcemap: true,
-        },
-        plugins: [purgeCss()],
+  },
+  vite: {
+    build: {
+      sourcemap: true,
     },
-    integrations: [
-        svelte(),
-        tailwind(),
-        icon(),
-        paraglide({ project: "./project.inlang", outdir: "./src/paraglide" }),
-    ],
+    plugins: [purgeCss()],
+  },
+  integrations: [
+    svelte(),
+    tailwind(),
+    icon(),
+    mdx(),
+    paraglide({
+      project: "./project.inlang",
+      outdir: "./src/paraglide",
+    }),
+  ],
 });
