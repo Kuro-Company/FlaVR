832243bd4e913126b775e4caad1027f980f6a030
0t4u <61939142+0t4u@users.noreply.github.com>
2025-03-07 10:20:21 +0000

    feat: animated bg

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,20 @@ export default {
         // screens,
         // fontSize,
         extend: {
+            animation: {
+                    marquee: "marquee 180s linear infinite",
+                    marquee2: "marquee2 180s linear infinite",
+                },
+            keyframes: {
+                marquee: {
+                    "0%": { transform: "translateX(0%)" },
+                    "100%": { transform: "translateX(-50%)" },
+                },
+                marquee2: {
+                    "0%": { transform: "translateX(-50%)" },
+                    "100%": { transform: "translateX(0%)" },
+                },
+            },
             colors: {
                 rosePine: {
                     base: "#191724",
