4aab4a8bf5970acc9532f796181565a9c494cb75
0t4u <61939142+0t4u@users.noreply.github.com>
2025-03-10 13:21:20 +0000

    chore: tweak borders

--- src/components/DarkModeToggleButton.svelte
+++ src/components/DarkModeToggleButton.svelte
@@ -20,7 +20,7 @@
 <!-- svelte-ignore a11y_role_has_required_aria_props -->
 <!-- svelte-ignore a11y_no_noninteractive_element_to_interactive_role -->
 <!-- svelte-ignore slot_element_deprecated -->
-<label role="radio" for={id} class={`flex items-center h-full cursor-pointer p-3 bg-rose-pine-dawn-highlight-med dark:bg-rose-pine-moon-highlight-med hover:bg-opacity-80" ${(right ? "rounded-r-md" : "")} ${(left ? "rounded-l-md" : "")} ${active ? " bg-rose-pine-dawn-highlight-high! dark:bg-rose-pine-moon-highlight-high!" : ""}`}>
+<label role="radio" for={id} class={`flex items-center h-full cursor-pointer p-3 bg-rose-pine-dawn-highlight-med dark:bg-rose-pine-moon-highlight-med hover:bg-opacity-80" ${(right ? "rounded-r-sm" : "")} ${(left ? "rounded-l-sm" : "")} ${active ? " bg-rose-pine-dawn-highlight-high! dark:bg-rose-pine-moon-highlight-high!" : ""}`}>
     <input id={id} name={name} type="radio" class="hidden border-hidden" checked={active} {onclick}>
     <slot />
 </label>
