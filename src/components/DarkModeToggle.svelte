8023ff7cbc299f57792b312b826be8b46afa3394
0t4u <61939142+0t4u@users.noreply.github.com>
2026-01-28 14:57:21 +0000

    feat: ui tweaks

--- src/components/DarkModeToggle.svelte
+++ src/components/DarkModeToggle.svelte
@@ -1,4 +1,7 @@
 <script lang="ts">
+    // stupid workaround for astro slots with no children ts error
+    let { children, sun, device, moon } = $props();
+
     import DarkModeToggleButton from "./DarkModeToggleButton.svelte";
     import { theme } from "~/util/themeStore.ts";
     import { changeTheme } from "~/util/changeTheme.ts";
@@ -17,13 +20,13 @@
 <div class="flex flex-row justify-end h-full">
     <div class="inline-flex flex-row" role="group">
         <DarkModeToggleButton id="sun" name="dmselect" left active={$theme === "light"} onclick={() => theme.set("light")}>
-            <slot name="sun" />
+            {@render sun()}
         </DarkModeToggleButton>
         <DarkModeToggleButton id="device" name="dmselect" active={$theme === "device"} onclick={() => theme.set("device")}>
-            <slot name="device" />
+            {@render device()}
         </DarkModeToggleButton>
         <DarkModeToggleButton id="moon" name="dmselect" right active={$theme === "dark"} onclick={() => theme.set("dark")}>
-            <slot name="moon" />
+            {@render moon()}
         </DarkModeToggleButton>
     </div>
 </div>
